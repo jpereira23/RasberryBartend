a6731a36f1125e8b74e397ffeeeb7f7a6a6248c6
Jeff Pereira <jefferypereira3@gmail.com>
2019-10-14 14:41:33 -0900

    Starting again

--- MainRobot.java
+++ MainRobot.java
@@ -21,22 +21,59 @@ public class MainRobot{
   }
 
   public static void main(String[] args) throws InterruptedException{
+    int[] intArray = new int[]{1,2,3,4};
     JFrame frame = new JFrame("Bartending Robot");
     JButton b = new JButton("Press");
 
     final GpioController gpio = GpioFactory.getInstance();
-    final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLed", PinState.HIGH);
+    final GpioPinDigitalOutput pin1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLed", PinState.LOW);
+    
+    final GpioPinDigitalOutput pin4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLed", PinState.LOW);
 
+    final GpioPinDigitalOutput pin5 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLed", PinState.LOW);
 
-    //pin.low();
-    pin.setShutdownOptions(true, PinState.LOW);
+    final GpioPinDigitalOutput pin6 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLed", PinState.LOW);
+
+    pin1.setShutdownOptions(true, PinState.LOW);
+
+    pin4.setShutdownOptions(true, PinState.LOW);
+
+    pin5.setShutdownOptions(true, PinState.LOW);
+
+    pin6.setShutdownOptions(true, PinState.LOW);
 
 
     b.addActionListener(new ActionListener()
     {
         public void actionPerformed(ActionEvent e)
         {
-          pin.toggle();
+          for(int i=0; i<4; i++){
+            pin1.low();
+            pin4.low();
+            pin5.low();
+            pin6.low();
+            switch(i){
+              case 0: 
+                pin1.high();
+                break; 
+              case 1: 
+                pin4.high();
+                break;  
+              case 2: 
+                pin5.high();
+                break;
+              case 3:
+                pin6.high();
+                break;
+              default: 
+                break;
+            } 
+            try{
+            Thread.sleep(intArray[i]*1000);
+            } catch (InterruptedException error){
+              System.out.println("Error");
+            }
+          }
         }
     });
 
