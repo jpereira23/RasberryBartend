15de595edb6cebf23e81b980f965290f6a2d758d
Jeffery Pereira <jefferypereira3@gmail.com>
2019-10-18 23:13:51 -0500

    Here

--- API.java
+++ API.java
@@ -2,14 +2,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.json.JSONObject;
 
 public class API{
   private String url;
   URL obj;
   HttpURLConnection con;
   public API(){
-    url =  = "http://138.197.205.247:8080/api/drinks";
+    url = "http://138.197.205.247:8080/api/drinks";
     obj = new URL(url);
     con = (HttpURLConnection) obj.openConnection();
     con.setRequestMethod("GET");
