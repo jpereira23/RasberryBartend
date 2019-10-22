fbd80bcf90d58f80715328e53fa4aeaf9c4bde97
Jeffery Pereira <jefferypereira3@gmail.com>
2019-10-21 21:05:41 -0500

    Hello

--- MainRobot.java
+++ MainRobot.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 // Main Robot Project
 
 public class MainRobot implements APIDelegate{ 
+  public API a = new API();    
   public static void main(String[] args) throws InterruptedException{
     int[] intArray = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
- 
     Robot r = new Robot();
-    GUI g = new GUI(a.drink1);
     g.b1.addActionListener(new ActionListener()
     {
       public void actionPerformed(ActionEvent e){
@@ -21,6 +20,6 @@ public class MainRobot implements APIDelegate{
 
   @Override
   public void finishedAPICall(){
-    API a = new API();    
+    GUI g = new GUI(a.drink1);
   } 
 }
