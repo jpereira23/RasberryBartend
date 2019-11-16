289bbafbb9003665affe935ecc9088001044ab88
Jeffery Pereira <jefferypereira3@gmail.com>
2019-11-16 11:15:05 -0600

    Fixed little issue

--- MainRobot.java
+++ MainRobot.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public class MainRobot{
   public static void main(String[] args){
-
+  	Robot robot = new Robot();
     ClientSocket client = new ClientSocket();
-    GUI gui = new GUI(client);
+    GUI gui = new GUI(client, robot);
     client.startConnection();
     client.print();
   }
