b6cc17d3398ca090025ed655c4e67548e5f033f7
Jeffery Pereira <jefferypereira3@gmail.com>
2019-12-23 20:18:11 -0600

    Hello World

--- RobotJava/MainPage/MainPage.java
+++ RobotJava/MainPage/MainPage.java
@@ -20,7 +20,7 @@ public class MainPage{
 		headerLabel = new JLabel("Please Select Drink");
 		headerLabel.setBounds(0, 0, 600, 50);
 		makeButton = new JButton("Make");
-		makeButton.setBounds(340, 415, 100, 50);
+		makeButton.setBounds(340, 365, 100, 50);
 		adminButton = new JButton("Admin"); 
 		adminButton.setBounds(600, 0, 200, 50);
 		component = new JPanel();
